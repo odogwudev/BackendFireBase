fdd96fae75d31a80c9c9855b49729338a6a5e578
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-02-18 23:48:55 +0100

    added retieve persons and realtime updates

--- app/src/main/java/com/odogwudev/kotlinbackendfirebase/cloudefirestore/FirebaseFirestoreActivity.kt
+++ app/src/main/java/com/odogwudev/kotlinbackendfirebase/cloudefirestore/FirebaseFirestoreActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.odogwudev.kotlinbackendfirebase.R
 import kotlinx.android.synthetic.main.activity_firebase_firestore.*
@@ -13,8 +14,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.lang.StringBuilder
 
-class FirebaseFirestoreActivity : AppCompatActivity(){
+class FirebaseFirestoreActivity : AppCompatActivity() {
 
     private val personCollectionRef = Firebase.firestore.collection("persons")
 
@@ -29,16 +31,69 @@ class FirebaseFirestoreActivity : AppCompatActivity(){
             val person = Person(firstName, lastName, age)
             savePerson(person)
         }
+
+        btnRetrieveData.setOnClickListener {
+            retrievePersons()
+        }
+
+        //realTimeUpdates()
     }
 
-    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch{
+    private fun realTimeUpdates() {
+        personCollectionRef.addSnapshotListener { querySnapshot, firebaseFireStoreException ->
+            firebaseFireStoreException?.let {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                return@addSnapshotListener
+            }
+            querySnapshot?.let {
+                val sb = StringBuilder()
+                for (document in it) {
+                    val person = document.toObject<Person>()
+                    sb.append("$person\n")
+                }
+                tvPersons.text = sb.toString()
+            }
+        }
+    }
+
+    private fun retrievePersons() = CoroutineScope(Dispatchers.IO).launch {
+        val fromAge = etFrom.text.toString()
+        val toAge = etTo.text.toString()
+        try {
+            val querySnapshot = personCollectionRef
+                .whereGreaterThan("age", fromAge)
+                .whereLessThan("age", toAge)
+         //       .whereEqualTo("firstname", "Michael")
+                .orderBy("age")
+                .get()
+                .await()
+            val sb = StringBuilder()
+            for (document in querySnapshot.documents) {
+                val person = document.toObject<Person>()
+                sb.append("$person\n")
+            }
+            withContext(Dispatchers.Main) {
+                tvPersons.text = sb.toString()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@FirebaseFirestoreActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
         try {
             personCollectionRef.add(person).await()
-            withContext(Dispatchers.Main){
-                Toast.makeText(this@FirebaseFirestoreActivity, "Suceesfully Saved Data", Toast.LENGTH_LONG).show()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(
+                    this@FirebaseFirestoreActivity,
+                    "Suceesfully Saved Data",
+                    Toast.LENGTH_LONG
+                ).show()
             }
-        } catch (e: Exception){
-            withContext(Dispatchers.Main){
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
                 Toast.makeText(this@FirebaseFirestoreActivity, e.message, Toast.LENGTH_LONG).show()
             }
         }
